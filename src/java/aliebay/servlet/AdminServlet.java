c37992260df0874b4cdd07dc03e8368ab16cf7ea
Jose-Tapia-Catena <jmtapiacatena@uma.es>
2022-04-25 10:06:26 +0200

    adminServlet

--- src/java/aliebay/servlet/AdminServlet.java
+++ src/java/aliebay/servlet/AdminServlet.java
@@ -4,6 +4,9 @@
  */
 package aliebay.servlet;
 
+import aliebay.dao.UsuarioFacade;
+import aliebay.entity.Usuario;
+import jakarta.ejb.EJB;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -11,6 +14,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  *
@@ -19,6 +23,7 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(name = "AdminServlet", urlPatterns = {"/AdminServlet"})
 public class AdminServlet extends AliEbaySessionServlet {
 
+    @EJB UsuarioFacade uf;
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -30,18 +35,13 @@ public class AdminServlet extends AliEbaySessionServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AdminServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AdminServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        if (super.comprobarSesion(request,response)){
+        List<Usuario> usuarios = uf.findAll();
+        
+        request.setAttribute("usuarios", usuarios);
+        
+        request.getRequestDispatcher("/WEB-INF/jsp/lista.jsp").forward(request, response);
         }
     }
 
